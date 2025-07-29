6b41140d01aef6b4ce2eeb999a8feda2010606f8
Catherine Lee <csl@fb.com>
2025-07-29 11:00:47 -0700

    tc

--- .ci/pytorch/win-test-helpers/installation-helpers/install_sccache.bat
+++ .ci/pytorch/win-test-helpers/installation-helpers/install_sccache.bat
@@ -6,8 +6,8 @@ if "%REBUILD%"=="" (
     del %TMP_DIR_WIN%\bin\sccache.exe || ver > nul
   )
   if "%BUILD_ENVIRONMENT%"=="" (
-    curl --retry 3 --retry-all-errors -k https://s3.amazonaws.com/ossci-windows/sccache-v0.10.0.exe --output %TMP_DIR_WIN%\bin\sccache.exe
+    curl --retry 3 --retry-all-errors -k https://s3.amazonaws.com/ossci-windows/sccache-8c07bf16821550692101b8db5855d930585bb3e9-x86_64-pc-windows-msvc.exe --output %TMP_DIR_WIN%\bin\sccache.exe
   ) else (
-    aws s3 cp s3://ossci-windows/sccache-v0.10.0.exe %TMP_DIR_WIN%\bin\sccache.exe
+    aws s3 cp s3://ossci-windows/sccache-8c07bf16821550692101b8db5855d930585bb3e9-x86_64-pc-windows-msvc.exe %TMP_DIR_WIN%\bin\sccache.exe
   )
 )
